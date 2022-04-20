211d46633b7fb992ee4c99e1375b7ef51dc6461e
TD <104011941+tdockter99@users.noreply.github.com>
2022-04-20 09:53:16 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
-streamlit.title('My Parents Healthy Diner')
+streamlit.title('My Mom\'s Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and Blueberry Oatmeal')
+streamlit.text ('Kale and Spinach Rocket Smoothie')
+streamlit.text('Hard Boiled Free-Range Egg')
